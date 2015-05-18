d26049baaf90df66aa4780169e63b16f36ca913d
Tobias Löb <g3neration0@gmail.com>
2015-05-18 14:30:15 +0200

    ADDED Menu as MessageReceiver

--- src/javascript/entry.js
+++ src/javascript/entry.js
@@ -4,11 +4,13 @@
  */
 var ClientSessionController = require('./nephelin/ClientSessionController');
 var mainpanel = require('./nephelin/mainpanel');
+var menu = require('./nephelin/menu');
 
 $( document ).ready(function() {
     console.log('ClientSessionController starting!');
     var csc = new ClientSessionController.csc('/NephelinServer');
     ClientSessionController.addReceiver(mainpanel.sp);
+    ClientSessionController.addReceiver(menu.Menu);
     csc.openConnection();
     csc.buildComponents();
     console.log('ClientSessionController started!');
