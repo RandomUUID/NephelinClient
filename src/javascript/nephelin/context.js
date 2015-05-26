705fac4a8a83c09a5741f751ab49be8fe96db44d
Tobias Löb <g3neration0@gmail.com>
2015-05-26 14:28:28 +0200

    CHANGED: Refactor Style Code

--- src/javascript/nephelin/context.js
+++ src/javascript/nephelin/context.js
@@ -3,41 +3,14 @@
  * Created by loeb on 19.05.2015.
  */
 
-var context;
+var Context;
 var Messages = require('./Messages');
-var Board = require('./Board');
-var self = this;
 
-
-function addContextItem(name, f) {
-    var item = document.createElement("li");
-    var context = document.createElement("ul");
-    item.innerHTML = name;
-    item.classList.add("context-item");
-    item.onclick = f;
-    $(".context-menu .list").append(item);
-}
-
-function removeContextItem(name) {
-    var items = $(".context-item");
-    try {
-        for (var i = 0; i < items.length; i += 1) {
-            if (items[i].innerText === name) {
-                items[i].remove();
-            }
-        }
-    }
-    catch
-        (e) {
-        console.log("menuItem not found: " + name);
-    }
-}
-
-context = function context(sendMessageFunc, socket) {
+Context = function Context(sendMessageFunc, socket) {
     console.log("Context Menu initialised");
     this.send = sendMessageFunc;
     this.socket = socket;
-    this.name = "menu";
+    this.name = "context";
 
     $(document).on('contextmenu', function (e) {
         $('.context-menu').css({
@@ -64,36 +37,61 @@ context = function context(sendMessageFunc, socket) {
     });
 
     //TODO dirty!
-    addContextItem("red", function () {
+    this.actions.addContextItem("red", function () {
         console.log("Red selected");
         var Board = require('./Board');
         Board.changeSelect("red");
     });
-    addContextItem("blue", function () {
+    this.actions.addContextItem("blue", function () {
         console.log("Blue selected");
         var Board = require('./Board');
         Board.changeSelect("blue");
     });
 };
 
-context.prototype = {
-    receive: function (msg) {
-        console.log("Module: " + this.name + " reached.");
-        var action = msg.action;
-        var p = msg.payload;
-        switch (action) {
-            case "addContextItem":
-                addContextItem(msg.payload, function()
-                {console.log("Clicked" + msg.payload);});
-                break;
-            case "removeContextItem":
-                removeContextItem(msg.payload);
-                break;
-            default :
-                console.log(msg);
-                this.send(Messages.ping);
+Context.prototype.actions = {};
+
+Context.prototype.actions.addContextItem = function addContextItem(name, f) {
+    var item = document.createElement("li");
+    var context = document.createElement("ul");
+    item.innerHTML = name;
+    item.classList.add("context-item");
+    item.onclick = f;
+    $(".context-menu .list").append(item);
+};
+
+Context.prototype.actions.removeContextItem = function removeContextItem(name) {
+    var $items = $(".context-item");
+    try {
+        for (var i = 0; i < $items.length; i += 1) {
+            if ($items[i].innerText === name) {
+                $items[i].remove();
+            }
         }
+    }
+    catch
+        (e) {
+        console.log("menuItem not found: " + name);
     }
 };
 
-module.exports.Context = context;
+Context.prototype.receive = function receive(msg) {
+    console.log("Module: " + this.name + " reached.");
+    var action = msg.action;
+    switch (action) {
+        case "addContextItem":
+            this.actions.addContextItem(msg.payload, function () {
+                console.log("Clicked" + msg.payload);
+            });
+            break;
+        case "removeContextItem":
+            this.actions.removeContextItem(msg.payload);
+            break;
+        default :
+            console.log(msg);
+            this.send(Messages.ping);
+    }
+};
+
+
+module.exports.Context = Context;
